10e553e98184a2d83340e6b7108369352d7acc49
Lee Provoost <lee.provoost@gmail.com>
2015-07-25 12:01:18 +0100

    changed data model, added users

--- README.md
+++ README.md
@@ -10,15 +10,23 @@ TODO
 
 ### Passport struct
 
-We are going to use a travel Passport for our example. I've chosen Id as the unique key for the passport because (in the UK), passport book numbers these days have a unique 9 character field length (e.g. 012345678).
+We are going to use a travel Passport for our example. I've chosen Id as the unique key for the passport because (in the UK), passport book numbers these days have a unique 9 character field length (e.g. 012345678). A passport belongs to a customer and a customer can have one or more passports.
 
 ```
-type Passport struct {
-  Id              string `json:"id"`
+type Customer struct {
+  Id              int    `json:"id"`
   FirstName       string `json:"first_name"`
   LastName        string `json:"last_name"`
   DateOfBirth     string `json:"date_of_birth"`
   LocationOfBirth string `json:"location_of_birth"`
+}
+
+type Passport struct {
+  Id           string `json:"id"`
+  DateOfIssue  string `json:"date_of_issue"`
+  DateOfExpiry string `json:"date_of_expiry"`
+  Authority    string `json:"authority"`
+  CustomerId   int    `json:"customer_id"`
 }
 ```
 
@@ -26,9 +34,11 @@ The first time you create a struct, you may not be aware that uppercasing and lo
 
 You may not want to expose your data to the consumer of your web service in this format, so you can override the way your fields are marshalled by adding ``json:"first_name"`` to each field with the desired name.
 
-## Inspired by
+## Useful references
 
+* https://github.com/codegangsta/negroni
 * http://vluxe.io/golang-web-api.html
 * https://github.com/msanterre/canoe/blob/master/main.go
 * http://alpacalunchbox.com/building-lightweight-apis-with-go/
-* https://gist.github.com/leeprovoost/879151cf696b52d6cecb
+* https://gist.github.com/danesparza/eb3a63ab55a7cd33923e
+* http://stackoverflow.com/questions/21825322/why-golang-cannot-generate-json-from-struct-with-front-lowercase-character
--- main.go
+++ main.go
@@ -8,21 +8,54 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Passport struct {
-	Id              string `json:"id"`
+type Customer struct {
+	Id              int    `json:"id"`
 	FirstName       string `json:"first_name"`
 	LastName        string `json:"last_name"`
 	DateOfBirth     string `json:"date_of_birth"`
 	LocationOfBirth string `json:"location_of_birth"`
 }
 
+type Passport struct {
+	Id           string `json:"id"`
+	DateOfIssue  string `json:"date_of_issue"`
+	DateOfExpiry string `json:"date_of_expiry"`
+	Authority    string `json:"authority"`
+	CustomerId   int    `json:"customer_id"`
+}
+
+func HomeHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Nothing to see here. #kthxbai")
+}
+
+func HealthcheckHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "HandleHealthchecks")
+}
+
+func UsersHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "Handling Users")
+}
+
 func PassportsHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "List of passports")
+	fmt.Fprintf(w, "Handling Passports")
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/passports", PassportsHandler).Methods("GET")
+	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/healthcheck", HealthcheckHandler).Methods("GET")
+
+	router.HandleFunc("/users", UsersHandler).Methods("GET")
+	router.HandleFunc("/users/{uid}", UsersHandler).Methods("GET")
+	router.HandleFunc("/users", UsersHandler).Methods("POST")
+	router.HandleFunc("/users/{uid}", UsersHandler).Methods("PUT")
+	router.HandleFunc("/users/{uid}", UsersHandler).Methods("DELETE")
+
+	router.HandleFunc("/users/{uid}/passports", PassportsHandler).Methods("GET")
+	router.HandleFunc("/passports/{pid}", PassportsHandler).Methods("GET")
+	router.HandleFunc("/users/{uid}/passports", PassportsHandler).Methods("POST")
+	router.HandleFunc("/passports/{pid}", PassportsHandler).Methods("PUT")
+	router.HandleFunc("/passports/{pid}", PassportsHandler).Methods("DELETE")
 
 	n := negroni.Classic()
 	n.UseHandler(router)
