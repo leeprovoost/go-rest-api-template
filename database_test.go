a42496f45ed7b3ee9fb6804a4e89df740da332c2
Lee Provoost <lee.provoost@gmail.com>
2015-08-20 09:04:59 +0100

    #6 implemented Get User tests, using testify package now

--- database_test.go
+++ database_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestMain(m *testing.M) {
@@ -19,13 +21,23 @@ func TestMain(m *testing.M) {
 func TestList(t *testing.T) {
 	var list map[string][]User = db.List()
 	var count = len(list["users"])
-	if count != 2 {
-		t.Errorf("Expected 2 elements in the list, only counting %v.", count)
+	assert.Equal(t, count, 2, "they should be equal")
+}
+
+func TestGetSuccess(t *testing.T) {
+	u, err := db.Get(0)
+	if assert.Nil(t, err) {
+		assert.Equal(t, u.Id, 0, "they should be equal")
+		assert.Equal(t, u.FirstName, "John", "they should be equal")
+		assert.Equal(t, u.LastName, "Doe", "they should be equal")
+		assert.Equal(t, u.DateOfBirth, "31-12-1985", "they should be equal")
+		assert.Equal(t, u.LocationOfBirth, "London", "they should be equal")
 	}
 }
 
-func TestGet(t *testing.T) {
-	t.Errorf("Test not implemented.")
+func TestGetFail(t *testing.T) {
+	_, err := db.Get(10)
+	assert.NotNil(t, err)
 }
 
 func TestAdd(t *testing.T) {
