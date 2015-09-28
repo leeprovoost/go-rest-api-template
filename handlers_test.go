b17e42507dee103efa5cd64eeecf98bebda13d62
Lee Provoost <lee.provoost@gmail.com>
2015-09-28 08:55:25 +0100

    #59 experiment with setup/teardown

--- handlers_test.go
+++ handlers_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -9,15 +10,28 @@ import (
 	"github.com/unrolled/render"
 )
 
-func TestListUsersHandler(t *testing.T) {
+func HandlersTestSetup() Env {
 	env := Env{
 		Metrics: stats.New(),
 		Render:  render.New(),
 	}
-	req, _ := http.NewRequest("GET", "http://localhost:3009/users", nil)
+	return env
+}
+
+func teardown() {
+
+}
+
+func TestListUsersHandler(t *testing.T) {
+	env := HandlersTestSetup()
+	req, _ := http.NewRequest("GET", "", nil)
 	w := httptest.NewRecorder()
 	makeHandler(env, ListUsersHandler).ServeHTTP(w, req)
+	fmt.Println(w)
 	if w.Code != http.StatusOK {
 		t.Errorf("Home page didn't return %v", http.StatusOK)
 	}
+	fmt.Println(w.Code)
+	fmt.Println(w.HeaderMap)
+	fmt.Println(w.Body)
 }
