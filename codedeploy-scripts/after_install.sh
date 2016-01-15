5bbff17f870f07f7d973ae8b80a7044b08c70873
Lee Provoost <lee.provoost@gmail.com>
2016-01-15 14:51:09 +0000

    appspec.yml file and scripts for codedeploy

---
+++ codedeploy-scripts/after_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# make golang workspace
+cd $HOME
+echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
+echo "export PATH=$PATH:$GOPATH/bin" >> $HOME/.bashrc
+
+# activate changes
+source $HOME/.bashrc
+
+# go to golang app
+cd $GOPATH/src/github.com/leeprovoost/go-rest-api-template
+
+# install dependencies
+go get -t -v ./...
+
+# make binary
+go install
+
+# copy fixtures,json file
+p /home/ec2-user/go/src/github.com/leeprovoost/go-rest-api-template/fixtures.json /home/ec2-user/go/bin
---
+++ codedeploy-scripts/application_start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+service go-rest-api-template start
