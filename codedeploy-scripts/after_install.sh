d3580bbd7b85b347b99c4e5c57eebb9b2d466e0f
Lee Provoost <lee.provoost@gmail.com>
2016-01-18 14:06:44 +0000

    deleted comments, added file exists checks

--- codedeploy-scripts/after_install.sh
+++ codedeploy-scripts/after_install.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-# make golang workspace
-#cd $HOME
-#echo "export GOPATH=$HOME/go" >> $HOME/.bashrc
-#echo "export PATH=$PATH:$GOPATH/bin" >> $HOME/.bashrc
-
-# activate changes
-#source $HOME/.bashrc
-
-# go to golang app
-#cd $GOPATH/src/github.com/leeprovoost/go-rest-api-template
-
-# install dependencies
-#go get -t -v ./...
-
-# make binary
-#go install
-
 # copy application binary
 cp $HOME/go/src/github.com/leeprovoost/go-rest-api-template/go-rest-api-template /opt/go-rest-api-template
 
--- codedeploy-scripts/application_stop.sh
+++ codedeploy-scripts/application_stop.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-# stop application
-kill -9 `cat /var/log/go-rest-api-template-pid.txt`
-
-# clean up old files
-rm -f /var/log/go-rest-api-template-pid.txt
-rm -f /var/log/go-rest-api-template.log
-rm -f /opt/go-rest-api-template/go-rest-api-template
-rm -f /opt/go-rest-api-template/fixtures.json
+if [ -f /var/log/go-rest-api-template-pid.txt ]; then
+  # Stop application
+  kill -9 `cat /var/log/go-rest-api-template-pid.txt`
+  # Then delete all files
+  rm -f /var/log/go-rest-api-template-pid.txt
+fi
+if [ -f /var/log/go-rest-api-template.log ]; then
+  rm -f /var/log/go-rest-api-template.log
+fi
+if [ -f /opt/go-rest-api-template/go-rest-api-template ]; then
+  rm -f /opt/go-rest-api-template/go-rest-api-template
+fi
+if [ -f /opt/go-rest-api-template/fixtures.json ]; then
+  rm -f /opt/go-rest-api-template/fixtures.json
+fi
