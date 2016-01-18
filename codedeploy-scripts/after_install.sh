78ac16bd4ea325f5ab897da3cda78045c799e3c6
Lee Provoost <lee.provoost@gmail.com>
2016-01-18 14:27:21 +0000

    added sudo to /opt install

--- codedeploy-scripts/after_install.sh
+++ codedeploy-scripts/after_install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# create app directory
+sudo mkdir /opt/go-rest-api-template
+
 # copy application binary
-cp $HOME/go/src/github.com/leeprovoost/go-rest-api-template/go-rest-api-template /opt/go-rest-api-template
+sudo cp $HOME/go/src/github.com/leeprovoost/go-rest-api-template/go-rest-api-template /opt/go-rest-api-template
 
 # copy fixtures,json file
-cp $HOME/go/src/github.com/leeprovoost/go-rest-api-template/fixtures.json /opt/go-rest-api-template
+sudo cp $HOME/go/src/github.com/leeprovoost/go-rest-api-template/fixtures.json /opt/go-rest-api-template
--- codedeploy-scripts/application_stop.sh
+++ codedeploy-scripts/application_stop.sh
@@ -3,19 +3,13 @@
 if [ -d $HOME/go/src/github.com/leeprovoost/go-rest-api-template]; then
   rm -rf $HOME/go/src/github.com/leeprovoost/go-rest-api-template
 fi
-
 if [ -f /var/log/go-rest-api-template-pid.txt ]; then
-  # Stop application
   kill -9 `cat /var/log/go-rest-api-template-pid.txt`
-  # Then delete all files
   rm -f /var/log/go-rest-api-template-pid.txt
 fi
 if [ -f /var/log/go-rest-api-template.log ]; then
   rm -f /var/log/go-rest-api-template.log
 fi
-if [ -f /opt/go-rest-api-template/go-rest-api-template ]; then
-  rm -f /opt/go-rest-api-template/go-rest-api-template
-fi
-if [ -f /opt/go-rest-api-template/fixtures.json ]; then
-  rm -f /opt/go-rest-api-template/fixtures.json
+if [ -d /opt/go-rest-api-template ]; then
+  rm -rf /opt/go-rest-api-template
 fi
