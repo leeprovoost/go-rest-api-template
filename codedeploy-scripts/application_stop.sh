895088682b15ed7c87e535761ff8532cab83432a
Lee Provoost <lee.provoost@gmail.com>
2016-01-18 09:02:21 +0000

    added -f to ignore non existing files

--- codedeploy-scripts/application_stop.sh
+++ codedeploy-scripts/application_stop.sh
@@ -4,8 +4,8 @@
 kill -9 `cat /var/log/go-rest-api-template-pid.txt`
 
 # clean up old files
-rm /var/log/go-rest-api-template-pid.txt
-rm /var/log/go-rest-api-template.log
+rm -f /var/log/go-rest-api-template-pid.txt
+rm -f /var/log/go-rest-api-template.log
 rm -rf $GOPATH/src/github.com/leeprovoost/go-rest-api-template
-rm /usr/bin/go-rest-api-template
-rm /home/ec2-user/fixtures.json
+rm -f /usr/bin/go-rest-api-template
+rm -f /home/ec2-user/fixtures.json
