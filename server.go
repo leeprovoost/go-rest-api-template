41514eaa1adbad3e219c19be4b3959338b303e20
Lee Provoost <lee.provoost@gmail.com>
2016-07-07 15:44:21 +0100

    improved usage of unrolled/secure

--- server.go
+++ server.go
@@ -22,8 +22,15 @@ func StartServer(ctx AppContext) {
 			Handler(handler)
 	}
 	// security
+	var isDevelopment = false
+	if ctx.Env == local {
+		isDevelopment = true
+	}
 	secureMiddleware := secure.New(secure.Options{
-		AllowedHosts: []string{}, // AllowedHosts is a list of fully qualified domain names that are allowed (CORS)
+		IsDevelopment:      isDevelopment, // This will cause the AllowedHosts, SSLRedirect, and STSSeconds/STSIncludeSubdomains options to be ignored during development. When deploying to production, be sure to set this to false.
+		AllowedHosts:       []string{},    // AllowedHosts is a list of fully qualified domain names that are allowed (CORS)
+		ContentTypeNosniff: true,          // If ContentTypeNosniff is true, adds the X-Content-Type-Options header with the value `nosniff`. Default is false.
+		BrowserXssFilter:   true,          // If BrowserXssFilter is true, adds the X-XSS-Protection header with the value `1; mode=block`. Default is false.
 	})
 	// start now
 	n := negroni.New()
